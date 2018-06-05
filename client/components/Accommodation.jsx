f9a4079559800a0c1bc9f9be53c87bf7b0a9b004
Julia <jnmareike@gmail.com>
2018-06-05 17:09:24 +1200

    add accommodation component

---
+++ client/components/Accommodation.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+class Accommodation extends React.Component {
+  constructor (props) {
+    super(props)
+    // this.state({
+
+    // })
+  }
+
+  calculateBenefit (amount, type) {
+
+  }
+
+  render () {
+    return (
+      <div>
+        <input type='number' />
+        <input type='button' value='Acommodation Supplement' />
+      </div>
+    )
+  }
+}
+
+export default Accommodation
