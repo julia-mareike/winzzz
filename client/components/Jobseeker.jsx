515ad36c61ab6f6af0ab91fa32bd441b278737cf
Julia <jnmareike@gmail.com>
2018-06-05 17:09:12 +1200

    move state to jobseeker component and logic to utils file

--- client/components/Jobseeker.jsx
+++ client/components/Jobseeker.jsx
@@ -1,33 +1,48 @@
 import React from 'react'
 
-const Jobseeker = props => {
-  let jobseeker = 'Jobseeker!'
-  const kids = props.state.tamariki === 'kids'
-  const single = props.state.relationship === 'single'
-  const working = props.state.partner === 'working'
-  const athome = props.state.housing === 'athome' && props.state.age === 'under20'
-  const late20splus = props.state.age === 'late20splus'
+import Section from './Section'
 
-  if (kids) {
-    if (!single) {
-      if (working) {
-        jobseeker = '205.60'
-      } else jobseeker = '192.25'
-    } else jobseeker = '334.05'
-  } else if (single) {
-    if (athome) {
-      jobseeker = '143.55'
-    } else if (late20splus) {
-      jobseeker = '215.34'
-    } else jobseeker = '179.44'
-  } else jobseeker = '179.44'
+import {jobseeker} from '../utils'
 
-  return (
-    <div>
-      <img className='jobseeker' src="face.png" alt="A nice face" />
-      {jobseeker}
-    </div>
-  )
+class Jobseeker extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick (e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render () {
+    const entitlement = jobseeker(this.state)
+    const singleNoKids = this.state.relationship === 'single' && this.state.tamariki === 'nokids'
+    const partnerAndKids = this.state.relationship === 'partner' && this.state.tamariki === 'kids'
+
+    return (
+      <div>
+        <img className='jobseeker' src="face.png" alt="A nice face" />
+        {entitlement}
+        <div className='tangata'>
+          <Section name='relationship' id={['single', 'partner']} handleClick={this.handleClick} />
+          <Section name='tamariki' id={['kids', 'nokids']} handleClick={this.handleClick} />
+
+          {partnerAndKids &&
+          <Section name='partner' id={['working', 'notworking']} handleClick={this.handleClick} />}
+
+          {singleNoKids &&
+          <Section name='age' id={['under20', 'early20s', 'late20splus']} handleClick={this.handleClick} />}
+
+          {(singleNoKids && this.state.age === 'under20') &&
+          <Section name='housing' id={['athome', 'awayfromhome']} handleClick={this.handleClick} />}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Jobseeker
---
+++ client/utils.js
@@ -0,0 +1,24 @@
+export const jobseeker = (state) => {
+  let jobseeker = 'Jobseeker!'
+  const kids = state.tamariki === 'kids'
+  const single = state.relationship === 'single'
+  const working = state.partner === 'working'
+  const athome = state.housing === 'athome' && state.age === 'under20'
+  const late20splus = state.age === 'late20splus'
+
+  if (kids) {
+    if (!single) {
+      if (working) {
+        jobseeker = '205.60'
+      } else jobseeker = '192.25'
+    } else jobseeker = '334.05'
+  } else if (single) {
+    if (athome) {
+      jobseeker = '143.55'
+    } else if (late20splus) {
+      jobseeker = '215.34'
+    } else jobseeker = '179.44'
+  } else jobseeker = '179.44'
+
+  return jobseeker
+}
